81e1fa74c372843b90d8d70cf525a4dc07065008
tslatt97 <87451853+tslatt97@users.noreply.github.com>
2021-07-30 16:06:16 +0200

    Delete src directory

--- src/main/java/com/example/eventmaster/places/Address.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eventmaster.places
-//Dataklasse for addresser
-data class Address(
-    val longName: String,
-    val shortName: String,
-    val type: ArrayList<String>)
--- src/main/java/com/example/eventmaster/places/InitializationException.kt
+++
@@ -1,3 +0,0 @@
-package com.example.eventmaster.places
-
-class InitializationException(message: String?) : Exception(message)
